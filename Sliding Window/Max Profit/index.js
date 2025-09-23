0703c118d3167610a3f45023ae4bd8a877d79b6a
Nishant Joshi <Nishant.Joshi1109@gmail.com>
2025-09-23 11:30:05 +0530

    sliding window in progress

---
+++ Sliding Window/Max Profit/index.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+    let maxProfit = 0;
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j <= i; j++) {
+            if (prices[i] === proce)
+        }
+
+    }
+};
+
+
+maxProfit([4, 2, 3, 1]);
+
+
+
